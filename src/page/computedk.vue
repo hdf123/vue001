ec4e5a446ac50eddc7ef117977ff9202f76939c2
thbts <1205552014@qq.com>
2019-06-06 16:39:22 +0800

    000

--- src/page/computedk.vue
+++ src/page/computedk.vue
@@ -1,12 +1,5 @@
 <template>
   <div class='computedk'>
-    <div id="example">
-      <p>Original message: "{{ message }}"</p>
-      <p>Computed reversed message: "{{ reversedMessage }}"</p>
-    </div>
-    <input type="text" v-model="childrens.name" />
-    <input type="text" v-model="lastName" />
-
     <table>
       <thead>
         <tr>
@@ -18,7 +11,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in list" :key="index">
+        <tr v-for="(item, ind) in list" :key="ind">
           <td>{{item.id}}</td>
           <td>{{item.name}}</td>
           <td>
@@ -34,46 +27,46 @@
       </tbody>
     </table>
     <p>一共{{totalCount}}件商品总价：{{totalPrice}}</p>
+
+    <hr>
+
+    <div v-for="(val,name,ind) in object" :key="ind">
+      <template v-if="ind==1">{{ind}}---{{name}}---{{val}}</template>
+      <template v-else-if="ind!=1">{{ind}}---{{name}}---{{val}}</template>
+    </div>
   </div>
 </template>
 
 <script>
-
   export default {
     name:'',
     props:[''],
     data () {
       return {
-        message: 'Hello',
-        childrens: {
-          name: '小强',
-          age: 20,
-          sex: '男'
-        },
-        tdArray:["1","2"],
-        lastName:"张三",
         list:[
           {id:'001',name:'苹果',price:100,count:2},
           {id:'002',name:'香蕉',price:200,count:6},
           {id:'003',name:'葡萄',price:300,count:9}
-        ]
+        ],
+        object: {
+          title: 'How to do lists in Vue',
+          author: 'Jane Doe',
+          publishedAt: '2016-04-10'
+        },
       };
     },
+
     components: {},
+
     computed: {
-      reversedMessage: function () {
-        return this.message.split('').reverse().join('')
-      },
-
-
-      totalCount:function(){
+      totalCount:function(){//总数量
         var totalCount=0;
         for(let i=0;i<this.list.length;i++){
           totalCount += this.list[i].count;
         }
         return totalCount;
       },
-      totalPrice:function(){
+      totalPrice:function(){//总价格
         var totalPrice=0;
         for(let i=0;i<this.list.length;i++){
           totalPrice += this.list[i].price * this.list[i].count;
@@ -84,7 +77,7 @@
 
     beforeMount() {},
 
-    mounted(){},
+    mounted() {},
 
     methods: {
       add:function(index){
@@ -99,28 +92,12 @@
       }
     },
 
-    watch: {
-      childrens:{
-        handler:function(val,oldval){
-          console.log(val.name)//46
-        },
-        deep:true//对象内部的属性监听，也叫深度监听
-      },
-      'childrens.name':function(val,oldval){
-        console.log(val+"aaa")//51
-      },//键路径必须加上引号
-      lastName:function(val,oldval){
-        console.log(this.lastName)
-      }
-    }
+    watch: {}
 
   }
 
 </script>
 <style lang='scss' scoped>
-*{
-  box-sizing: border-box;
-}
   .computedk{
     background:rgba(0,0,0,0.5);
   }
@@ -130,7 +107,7 @@
       width:100%;
       >td{
         text-align: center;
-        border:1px solid rgba(0,0,0,0.5);
+        // border:1px solid rgba(0,0,0,0.5);
       }
       >td:nth-child(1){
         width:10%;
@@ -140,9 +117,11 @@
       }
       >td:nth-child(3){
         width:35%;
+        padding:0 10px;
         >div{
           width:100%;
           display: flex;
+          align-items: center;
           justify-content: space-between;
         }
       }
